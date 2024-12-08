e739fe4877fd7b3e6b042fdd9ced905e3df64044
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2024-12-08 18:43:48 +0800

    first commit

---
+++ src/main/java/com/mycompany/gameburza/GameBurza.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gameburza;
+
+/**
+ *
+ * @author sean
+ */
+public class GameBurza {
+
+    public static void main(String[] args) {
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.show();
+        System.out.println("Login");
+    }
+}
