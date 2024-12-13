4fd757a67fbe4d2d6361c572b0c22ea0d45a6062
kurikurimaw <2023-101371@rtu.edu.ph>
2024-12-13 08:00:44 +0800

    fixed error when using icons

--- src/main/java/com/mycompany/gameburza/LoginScreen.java
+++ src/main/java/com/mycompany/gameburza/LoginScreen.java
@@ -144,7 +144,7 @@ public class LoginScreen extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(700, 700, 700))
         );
         layout.setVerticalGroup(
